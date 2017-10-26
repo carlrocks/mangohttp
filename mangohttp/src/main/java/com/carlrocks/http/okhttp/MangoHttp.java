c37d197b7ed650601c545960677824bddb1c9a68
ldf714@sina.com <ldf714@sina.com>
2017-10-26 11:06:03 +0800

    update MangoHttp

--- mangohttp/src/main/java/com/carlrocks/http/okhttp/MangoHttp.java
+++ mangohttp/src/main/java/com/carlrocks/http/okhttp/MangoHttp.java
@@ -6,9 +6,7 @@ import com.carlrocks.http.okhttp.callback.FileCallBack;
 import com.carlrocks.http.okhttp.exception.NetRequestException;
 import com.carlrocks.http.okhttp.https.HttpsUtils;
 import com.carlrocks.http.okhttp.utils.MangoLog;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Response;
+import okhttp3.*;
 
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.X509TrustManager;
@@ -58,6 +56,38 @@ public final class MangoHttp {
 	public static <T> void get(String url, final Class<T> clazz,
                                 final NetRequestParameters parameters, final RequestCallback<T> callBack) {
 		MangoHttpUtils.get().url(url).tag(url).paramsData(parameters.getParams()).build().execute(new Callback() {
+			@Override
+			public Object parseNetworkResponse(Response response, int id) throws Exception {
+				return response.body().string();
+			}
+
+			@Override
+			public void onError(Call call, Exception e, int id) {
+				if (callBack != null) callBack.onException(new NetRequestException(e.getMessage()));
+			}
+
+			@Override
+			public void onResponse(Object response, int id) {
+				String responseStr = response.toString();
+				logResponse(responseStr);
+				T t = JSON.parseObject(responseStr, clazz);
+				if (callBack != null) callBack.done(t);
+			}
+		});
+	}
+
+
+	/**
+	 * postContent
+	 * @param url
+	 * @param clazz
+	 * @param content
+	 * @param callBack
+	 * @param <T>
+	 */
+	public static <T> void postContent(String url, MediaType mediaType, final Class<T> clazz,
+							   final String content, final RequestCallback<T> callBack) {
+		MangoHttpUtils.postString().url(url).mediaType(mediaType).content(content).build().execute(new Callback() {
 			@Override
 			public Object parseNetworkResponse(Response response, int id) throws Exception {
 				return response.body().string();
